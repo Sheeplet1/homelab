d2c6d430d8911763310502f0f3ec354331309a6e
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2025-01-22 20:43:13 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Homelab NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }@inputs:
+    let
+      nodes = [
+        "homelab-0"
+      ];
+    in
+    {
+      nixosConfigurations = builtins.listToAttrs (
+        map (name: {
+          name = name;
+          value = nixpkgs.lib.nixosSystem {
+            specialArgs = {
+              meta = {
+                hostname = name;
+              };
+            };
+            system = "x86_64-linux";
+            modules = [
+              ./hardware-configuration.nix
+              ./configuration.nix
+            ];
+          };
+        }) nodes
+      );
+    };
+}
